c9d4d21c93dead415108e0229c9b7cd20ad49a95
FacundoCGM <114027262+FacundoCGM@users.noreply.github.com>
2024-07-16 14:50:10 -0300

    nativeQuery
    Se agrega el nativeQuery = true en autorRepository

--- literalura/src/main/java/com/alura/literalura/repository/AutorRepository.java
+++ literalura/src/main/java/com/alura/literalura/repository/AutorRepository.java
@@ -3,13 +3,16 @@ package com.alura.literalura.repository;
 import com.alura.literalura.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+
+
 public interface AutorRepository extends JpaRepository<Autor, Long> {
     Optional<Autor> findByNombre(String nombre);
 
-    @Query("SELECT * FROM autor WHERE fecha_de_nacimiento < :fechaDeEstarVivo AND fecha_de_fallecimiento < :fechaDeEstarVivo")
+    @Query(value = "SELECT * FROM autor WHERE fecha_de_nacimiento < :fechaDeEstarVivo AND fecha_de_fallecimiento < :fechaDeEstarVivo", nativeQuery = true)
     List<Autor> autorEstaVivo(int fechaDeEstarVivo);
 }
--- literalura/src/main/java/com/alura/literalura/repository/LibroRepository.java
+++ literalura/src/main/java/com/alura/literalura/repository/LibroRepository.java
@@ -2,9 +2,11 @@ package com.alura.literalura.repository;
 
 import com.alura.literalura.model.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+
 
 public interface LibroRepository extends JpaRepository<Libro,Long> {
     List<Libro> findByIdiomas(String idiomas);
